51cf8ec602068526d6979741d72eafe227e144d5
oshikiri <oshikiri@users.noreply.github.com>
2016-12-09 17:19:23 +0900

    不要な空白を削除

--- sembei/evaluate.py
+++ sembei/evaluate.py
@@ -10,21 +10,21 @@ def evaluate_by_jawordsim(path_jawordsim, vectors, scatter_plot=False):
     scatter_plot
     '''
     similarities_ja = pd.read_csv(path_jawordsim, skipinitialspace=True)
-    
+
     similarities_predict = []
     for i_row, row in similarities_ja.iterrows():
         if row.word1 in vectors.index and row.word2 in vectors.index:
             similarity = np.dot(vectors.loc[row.word1].values, vectors.loc[row.word2].values)
         else:
             similarity = None
-        
+
         similarities_predict.append(similarity)
-        
+
     similarities_ja.loc[:, 'predict'] = pd.Series(similarities_predict)
-    
+
     correlation = similarities_ja.corr(method='spearman').loc['mean', 'predict']
     coverage = similarities_ja.predict.notnull().mean()
-    
+
     if scatter_plot:
         similarities_ja.plot.scatter('predict', 'mean', figsize=(5, 5))
 
