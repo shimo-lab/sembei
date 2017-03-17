786101fa5b5247d9e97a2aed4238d578e7927c60
oshikiri <oshikiri@users.noreply.github.com>
2017-03-17 18:25:19 +0900

    versionを追加

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ path_cython = ['sembei/embed_inner.pyx', 'sembei/utils/counting.pyx']
 
 setup(
     name='sembei',
+    version='0.1',
     packages=['sembei', 'sembei.utils'],
     ext_modules=cythonize(path_cython),
     include_dirs=[np.get_include()]
